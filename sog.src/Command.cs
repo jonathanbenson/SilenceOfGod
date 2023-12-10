46731ea7cc02d1e1499725f6025b889c700d0342
Jonathan Benson <jbensonamdg@gmail.com>
2023-12-09 18:05:58 -0600

    Replace words before execution of commands

--- sog.src/Command.cs
+++ sog.src/Command.cs
@@ -4,25 +4,6 @@ using System.Windows;
 
 namespace sog.src;
 
-class CommandExecuter
-{
-    public string Name { get; }
-
-    protected Action<string[]> UiCallback { get; }
-
-    public CommandExecuter(string name, Action<string[]> uiCallback)
-    {
-        Name = name;
-        UiCallback = uiCallback;
-    }
-
-    public void Execute(string[] args)
-    {
-        UiCallback(args);
-    }
-
-}
-
 public class CommandDispatcher
 {
     private ICollection<CommandExecuter> CommandExecuters = new List<CommandExecuter>();
@@ -54,4 +35,44 @@ public class CommandDispatcher
     {
         CommandExecuters.Add(new CommandExecuter(commandName, uiCallback));
     }
+}
+
+class CommandExecuter
+{
+    public string Name { get; }
+
+    protected Action<string[]> UiCallback { get; }
+
+    public CommandExecuter(string name, Action<string[]> uiCallback)
+    {
+        Name = name;
+        UiCallback = uiCallback;
+    }
+
+    public void Execute(string[] args)
+    {
+        for (int i = 0; i < args.Length; i++)
+            foreach (Tuple<string, string> replacement in ArgReplacements)
+                args[i] = args[i].Replace(replacement.Item1, replacement.Item2);
+
+        UiCallback(args);
+    }
+
+    private static IEnumerable<Tuple<string, string>> ArgReplacements = new List<Tuple<string, string>>
+    {
+        new Tuple<string, string>("first", "1"),
+        new Tuple<string, string>("second", "2"),
+        new Tuple<string, string>("third", "3"),
+        new Tuple<string, string>("one", "1"),
+        new Tuple<string, string>("two", "2"),
+        new Tuple<string, string>("three", "3"),
+        new Tuple<string, string>("four", "4"),
+        new Tuple<string, string>("five", "5"),
+        new Tuple<string, string>("six", "6"),
+        new Tuple<string, string>("seven", "7"),
+        new Tuple<string, string>("eight", "8"),
+        new Tuple<string, string>("nine", "9"),
+        new Tuple<string, string>("ten", "10"),
+    };
+
 }
