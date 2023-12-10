aba54b9f988446be420f30af60feba99f11cefcd
Jonathan Benson <jbensonamdg@gmail.com>
2023-12-10 14:48:46 -0600

    Restructure project some more

--- sog.test/BibleBuilderTest.cs
+++ sog.test/BibleBuilderTest.cs
@@ -1,5 +1,5 @@
 
-using sog.src.model;
+using sog.src;
 
 namespace sog.test;
 
@@ -19,9 +19,20 @@ public class BibleBuilderTest
         Book exodus = bible.books[1];
         Book leviticus = bible.books[2];
 
-        BookBuilderTest bookBuilderTest = new BookBuilderTest();
+        Assert.AreEqual("Genesis", genesis.book);
+        Assert.AreEqual("Exodus", exodus.book);
+        Assert.AreEqual("Leviticus", leviticus.book);
 
-        bookBuilderTest.AssertFirst3Books(genesis, exodus, leviticus);
+        Assert.AreEqual(50, genesis.chapters.Count);
+        Assert.AreEqual(40, exodus.chapters.Count);
+        Assert.AreEqual(27, leviticus .chapters.Count);
 
+        string genesis11 = "In the beginning God created the heaven and the earth.";
+        string exodus11 = "Now these are the names of the children of Israel, which came into Egypt; every man and his household came with Jacob.";
+        string leviticus11 = "And the LORD called unto Moses, and spake unto him out of the tabernacle of the congregation, saying,";
+
+        Assert.AreEqual(genesis11, genesis.chapters[0].verses[0].text);
+        Assert.AreEqual(exodus11, exodus.chapters[0].verses[0].text);
+        Assert.AreEqual(leviticus11, leviticus.chapters[0].verses[0].text);
     }
 }
